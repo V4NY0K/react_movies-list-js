d3fd487f9499b78019a116c1fe17702e08d3b602
Ivan <igrytsaenko@gmail.com>
2026-01-12 18:39:59 +0100

    Solution 2

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,10 +1,10 @@
 import { MovieCard } from '../MovieCard';
 import './MovieList.scss';
 
-export const MovieList = ({ moviesFromServer }) => {
+export const MovieList = ({ movies }) => {
   return (
     <div className="movies">
-      {moviesFromServer.map(movie => (
+      {movies.map(movie => (
         <MovieCard movie={movie} key={movie.imdbId} />
       ))}
     </div>
